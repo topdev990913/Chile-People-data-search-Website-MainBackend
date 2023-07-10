54deb54b1b5611bd5107d3850ee911d99ab5b3c2
TopDevelpoer <133781576+PETER990913@users.noreply.github.com>
2023-07-10 16:40:39 -0700

    little update

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ let bodyParser = require('body-parser')
 let dbConfig = require('./database/db')
 
 // Express Route
-const studentRoute = require('../backend/routes/student.route')
+const studentRoute = require('./routes/student.route')
 
 // Connecting mongoDB Database
 mongoose.Promise = global.Promise
