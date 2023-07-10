1ccf5d7907b1c849957f10a9e7766cbdfd1144ba
TopDevelpoer <133781576+PETER990913@users.noreply.github.com>
2023-07-10 16:28:17 -0700

    Update db.js

--- database/db.js
+++ database/db.js
@@ -1,3 +1,4 @@
 module.exports = {
-  db: 'mongodb://localhost:27017/People',
+  // db: 'mongodb://localhost:27017/People',
+  db: "mongodb+srv://jeremyt1132:AlBsrejO9uAgtJIM@freedb.yul9lsv.mongodb.net/People"
 }
